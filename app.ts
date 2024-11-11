c01db5d976ecde59ef0731d0c3337ff0104cb400
Hafizh Fadhlurrohman <20598933+hafizhfadh@users.noreply.github.com>
2024-11-11 20:56:17 +0700

    Update docker configuration to meet server specs

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+<<<<<<<< HEAD:app.ts
 import { Hono } from 'hono'
 import { logger } from "hono/logger";
 
@@ -7,3 +8,10 @@ app.use('*', logger())
 app.get('/', (c) => c.text('Hono!'))
 
 export default app
+========
+import app from "./app";
+
+Bun.serve({
+  fetch: app.fetch
+});
+>>>>>>>> adac8f3 (Update docker configuration to meet server specs):src/index/app.ts
