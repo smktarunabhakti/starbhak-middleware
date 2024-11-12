1e9cd067e63b69d433ad8537606e8581f3c8b081
Hafizh Fadhlurrohman <20598933+hafizhfadh@users.noreply.github.com>
2024-11-12 20:50:00 +0700

    fix: test branch ruleset

--- src/index/app.ts
+++ src/index/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import { Hono } from 'hono'
+import { logger } from "hono/logger";
+
+const app = new Hono()
+
+app.use('*', logger())
+app.get('/', (c) => c.text('Hono!'))
+
+export default app
