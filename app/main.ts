3323686be2efe58f1e6f2c7e031c928be977305c
Hafizh Fadhlurrohman <20598933+hafizhfadh@users.noreply.github.com>
2024-11-13 11:20:03 +0700

    Feature/project structure (#2)
    * fix: update .gitignore to .gitkeep to securely save the folder
    
    * fix: fixing app flow by structure
    
    * fix: fixing app flow by structure
    
    * fix: fixing app flow by structure

---
+++ app/main.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
+
+const app = new Hono();
+
+app.use("*", logger());
+
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
+
+app.get("/", (c) => c.text('Hono!'))
+
+export default app;
