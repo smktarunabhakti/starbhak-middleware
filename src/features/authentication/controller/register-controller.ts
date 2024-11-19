5b10ce83624d51435d2800418a4368bd9ff51b86
HilmiAnarya <hilmidrone@gmail.com>
2024-11-19 11:35:39 +0700

    update: validation added to register controller

--- src/features/authentication/controller/register-controller.ts
+++ src/features/authentication/controller/register-controller.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 import { registerService } from "../service/register-service.ts";
-import { errorResponse, successResponse } from "../../../common/utils/api-response";
 import { users } from "../../../db/migrations/users-table-schema.ts";
 import { db } from "../../../db";
 import { and, eq } from "drizzle-orm";
+import registerValidation from "../validation/register-validation";
+import type { z } from "zod";
+import {
+    errorResponse,
+    successResponse,
+} from "../../../common/utils/api-response";
 
 type BlankEnv = {}; // Replace with actual environment type if needed
 type BlankSchema = {}; // Replace with actual schema type if needed
@@ -11,10 +16,25 @@ type BlankSchema = {}; // Replace with actual schema type if needed
 const registerController: Hono<BlankEnv, BlankSchema, "/"> = new Hono();
 
 registerController.post("/", async (c) => {
+
     const { email, name, passwordHash } = await c.req.json();
 
-    if (!email || !name ||!passwordHash) {
-        return c.json(errorResponse("Email dan password dibutuhkan!"), 400);
+    try {
+        registerValidation.parse({
+            email: email,
+            password: passwordHash,
+            name: name,
+        });
+    } catch (error) {
+        return c.json(
+            errorResponse(
+                "Data tidak terdaftar!",
+                (error as z.ZodError).errors.map((e) => ({
+                    field: e.path[0],
+                    message: e.message,
+                }))
+            )
+        );
     }
 
     const registerResult = await registerService(email, name, passwordHash);
