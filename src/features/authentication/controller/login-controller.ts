71a03030fe6bc1c97018fbf93bf18a85eb6a0069
yyypluto <140881883+yayayapluto@users.noreply.github.com>
2024-11-21 16:17:05 +0700

    Feature/authentication login 86eqqubcd (#7)
    * update: sample login feature
    
    * update: login route done, also with schema too
    
    * update: create user, userRole, role, rolePermission, permission interface and model
    
    * update: validation added to login controller
    
    * update: login tested done, but no hashing check yet
    
    * update: store login token to user

---
+++ src/features/authentication/controller/login-controller.ts
@@ -0,0 +1,75 @@
+import { Hono } from "hono";
+import { loginService } from "../service/login-service";
+import type { JWTPayload } from "hono/utils/jwt/types";
+import { jwt, sign } from "hono/jwt";
+import {
+  errorResponse,
+  successResponse,
+} from "../../../common/utils/api-response";
+import { getUserByEmail, updateUser } from "../../../common/model/user-model";
+import loginValidation from "../validation/login-validation";
+import type { z } from "zod";
+const loginController = new Hono();
+
+loginController.post("/", async (c) => {
+  const { email, password } = await c.req.json();
+
+  if (!email || !password) {
+    return c.json(errorResponse("Email dan password dibutuhkan!"), 400);
+  }
+
+  try {
+    loginValidation.parse({
+      email: email,
+      password: password,
+    });
+  } catch (error) {
+    return c.json(
+      errorResponse(
+        "Email atau password tidak valid!",
+        (error as z.ZodError).errors.map((e) => ({
+          field: e.path[0],
+          message: e.message,
+        }))
+      )
+    );
+  }
+
+  const loginResult = await loginService(email, password);
+
+  if (!process.env.X_SECRET) {
+    return c.json(errorResponse("Tidak bisa membuat token JWT"), 500);
+  }
+
+  if (!loginResult.success) {
+    return c.json(errorResponse(loginResult.message), 500);
+  }
+
+  const { id } = await getUserByEmail(email);
+  const exp = Math.floor(Date.now() / 1000) + (7 * 24 * 60 * 60 * 1000) / 1000;
+
+  const token = await sign(
+    {
+      id: id,
+      exp: exp,
+    } as JWTPayload,
+    process.env.X_SECRET
+  );
+
+  if (!id) {
+    return c.json(
+      errorResponse("Tidak bisa menemukan id"),
+      500
+    )
+  }
+
+  await updateUser(id, {
+    lastLoginAt: new Date(),
+    refreshTokenHash: token,
+    updatedAt: new Date()
+  })
+
+  return c.json(successResponse(loginResult.message, {token: token}), 200);
+});
+
+export default loginController;
