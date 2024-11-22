fb17f091df1b5ec92ef6371aa5186742d5918096
yyypluto <140881883+yayayapluto@users.noreply.github.com>
2024-11-22 20:35:18 +0700

    Feature/authentication logout 86eqqube3 (#8)
    * update: sample login feature
    
    * update: login route done, also with schema too
    
    * update: create user, userRole, role, rolePermission, permission interface and model
    
    * update: register
    
    * update: logout
    
    ---------
    
    Co-authored-by: Daffa Arief <hilmidrone@gmail.com>

--- src/features/authentication/routes/auth-route.ts
+++ src/features/authentication/routes/auth-route.ts
@@ -2,15 +2,17 @@ import { Hono } from "hono";
 import refreshTokenController from "../controller/refreshToken-controller";
 import loginController from '../controller/login-controller';
 import registerController from '../controller/register-controller';
+import logoutController from "../controller/logout-controller";
 import ResetPass from "../controller/reset-password-controller";
 import kodeOtp from "../controller/kode-otp-controller";
 
 const authRoute = new Hono()
 
 authRoute.route("/login", loginController)
+authRoute.route("/logout", logoutController)
+authRoute.route("/refresh-token", refreshTokenController)
 authRoute.route("/register", registerController)
 authRoute.route("/reset-password", ResetPass)
 authRoute.route("/kode-otp", kodeOtp)
-authRoute.route("/refresh-token", refreshTokenController)
 
 export default authRoute
---
+++ src/features/authentication/service/logout-service.ts
@@ -0,0 +1,17 @@
+import { db } from "../../../db";
+import { users } from "../../../db/migrations/users-table-schema.ts";
+import {eq} from "drizzle-orm"
+
+export const removeToken = async (userId: string): Promise<boolean> => {
+    try {
+        // Update the user record to set the token to null
+        await db
+            .update(users)
+            .set({refreshTokenHash: null})
+            .where(eq(users.id, userId));  // Find by userId
+        return true;
+    } catch (error) {
+        console.error('Error removing token:', error);
+        return false;
+    }
+}
