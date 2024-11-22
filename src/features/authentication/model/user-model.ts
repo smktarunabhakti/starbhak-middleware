1ce5638e4d171d3fb6625057198fe2f1734c910f
yyypluto <140881883+yayayapluto@users.noreply.github.com>
2024-11-22 19:57:25 +0700

    Feature/authentication reset password 86eqqubg2 (#11)
    * feature/reset-password : beloman loh ya
    
    * reset-password done
    
    * fix conflict
    
    * bugfix: bun.lockb
    
    ---------
    
    Co-authored-by: Briantoroflh <briantoroflh@gmail.com>
    Co-authored-by: HilmiAnarya <76718095+HilmiAnarya@users.noreply.github.com>

--- src/features/authentication/model/user-model.ts
+++ src/features/authentication/model/user-model.ts
@@ -1,26 +1,85 @@
-interface User {
-  name: string;
-  password: string;
-}
+import { eq } from "drizzle-orm";
+import { db } from "../../db";
+import { users } from "../../db/schema";
+import type { User } from "../interfaces/user-interface";
 
-const users: User[] = [
-    {
-        name: "Farras",
-        password: "ganteng"
-    },
-    {
-        name: "Reza",
-        password: "ganteng"
-    },
-    {
-        name: "Andhika",
-        password: "ganteng"
-    },
-]
+const getAllUsers = async (): Promise<User[]> => {
+    const collections = await db.select().from(users);
+    return collections as User[];
+};
 
-export const getAllUsers = (): User[] => users
+const getUserById = async (id: string): Promise<User> => {
+    const user = await db.select().from(users).where(eq(users.id, id)).limit(1);
+    return user[0] as User;
+};
 
-export const getUserByName = (name: string): User | undefined => users.find(user => user.name === name)
+const getUserByEmail = async (email: string): Promise<User> => {
+    const user = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email))
+        .limit(1);
+    return user[0] as User;
+};
 
+const getUserByName = async (name: string): Promise<User> => {
+    const user = await db.select().from(users).where(eq(users.name, name));
+    return user[0] as User;
+};
 
+const createUser = async (
+    email: string,
+    passwordHash: string,
+    name: string,
+    roleId: string
+): Promise<User> => {
+    const newUser = await db
+        .insert(users)
+        .values({
+            email,
+            passwordHash,
+            name,
+            roleId,
+        })
+        .returning();
+    return newUser[0] as User;
+};
 
+const updateUser = async (
+    id: string,
+    data: {
+        email?: string;
+        passwordHash?: string;
+        name?: string;
+        roleId?: string;
+        isActive?: boolean;
+        lastLoginAt?: Date;
+        refreshTokenHash?: string;
+        updatedAt?: Date;
+    }
+): Promise<User> => {
+    const updatedUser = await db
+        .update(users)
+        .set(data)
+        .where(eq(users.id, id))
+        .returning();
+    return updatedUser[0] as User;
+};
+
+const deleteUser = async (id: string): Promise<User> => {
+    const deletedUser = await db
+        .delete(users)
+        .where(eq(users.id, id))
+        .returning();
+    return deletedUser[0] as User;
+};
+
+export {
+    getAllUsers,
+    getUserById,
+    getUserByEmail,
+    getUserByName,
+    createUser,
+    updateUser,
+    deleteUser,
+};
--- src/features/authentication/routes/auth-route.ts
+++ src/features/authentication/routes/auth-route.ts
@@ -2,11 +2,16 @@ import { Hono } from "hono";
 import refreshTokenController from "../controller/refreshToken-controller";
 import loginController from '../controller/login-controller';
 import registerController from '../controller/register-controller';
+import ResetPass from "../controller/reset-password-controller";
+import kodeOtp from "../controller/kode-otp-controller";
+
 
 const authRoute = new Hono()
 
 authRoute.route("/login", loginController)
-authRoute.route("/refresh-token", refreshTokenController)
 authRoute.route("/register", registerController)
+authRoute.route("/reset-password", ResetPass)
+authRoute.route("/kode-otp", kodeOtp)
+authRoute.route("/refresh-token", refreshTokenController)
 
 export default authRoute
