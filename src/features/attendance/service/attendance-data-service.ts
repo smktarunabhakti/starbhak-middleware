dd6b40882fcc67cbc315fd37be393e4dfd05bd1e
Adri-Bagas <abagaswitjaksono@gmail.com>
2024-12-09 20:18:02 +0700

    fix: get self record

--- src/features/attendance/service/attendance-data-service.ts
+++ src/features/attendance/service/attendance-data-service.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm"
 import { db } from "../../../db"
 import { attendanceRecord } from "../../../db/schemas/attendance-records-table-schema"
 import { student } from "../../../db/schemas/students-table-schema"
@@ -19,13 +20,13 @@ export async function GetAllAttendanceRecord() {
 export async function GetSelfAttendanceRecord(id: string) {
     try{
 
-        const findStudent = await db.select().from(student).where()
+        const findStudent = await db.select().from(student).where(eq(student.user_id, id))
 
-        const datas = await db.select().from(attendanceRecord).where(eq(attendanceRecord.student_id))
+        const datas = await db.select().from(attendanceRecord).where(eq(attendanceRecord.student_id, findStudent[0].student_id!))
 
         return {
             success: true,
-            message: "Getted All!",
+            message: "Getted yourself!",
             data: datas
         }
     }catch(e){
